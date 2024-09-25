d2a1da0559c7128c67bfbd4c3bb09d1d32bdb05c
Alicja <alanotowska@yahoo.co.uk>
2024-09-25 22:54:06 +0200

    Implements the Clone trait for CalculatorMessage

--- src/message.rs
+++ src/message.rs
@@ -1,4 +1,10 @@
-#[derive(Debug)]
+// Messages should be Cloned because they represent pure data/events
+// button needs it too, because there is a chance that you will press it multiple times
+// between calls
+
+// Defines the types of messages or events that
+// the calculator will handle
+#[derive(Debug, Clone)]
 pub enum CalculatorMessage {
     Calculate
 }
