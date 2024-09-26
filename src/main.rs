fe0aadec7caa1d2d7da896170e0628bed2f466c4
Alicja <alanotowska@yahoo.co.uk>
2024-09-26 14:21:05 +0200

    Changes theme and text on press

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,81 @@
 #![allow(warnings)]
 
-mod message;
+// mod message;
 
-use iced::{widget::{self, text}, Sandbox, Settings, Theme};
+use iced::{
+    widget::{self, button, column, container, text},
+    Sandbox, Settings, Theme,
+};
 
-fn main() {
-    Calculator::run(Settings::default());
+fn main() -> iced::Result {
+    TextPrinter::run(Settings::default())
 }
 
-struct Calculator {
-    opening: String,
+struct TextPrinter {
+    text: String,
+    theme: Theme,
+    base_title: String,
 }
 
-impl Sandbox for Calculator {
-    type Message = message::CalculatorMessage;
+#[derive(Debug, Clone, PartialEq)]
+pub enum PrinterMessage {
+    Print,
+    ChangeTheme,
+}
 
-    // notatka:
-    // kompilator krzyczał gdy było tutaj "fn new() -> Self {}",
-    // bo gdy scope jest pusty to evaluuje się do () (unit type),
-    // a przecież obiecałaś mu, że zwrócisz Self (czyli Calculator)
+impl Sandbox for TextPrinter {
+    type Message = PrinterMessage;
 
-    // kompilator krzyczał jeśli zrobisz "fn new() {}",
-    // bo mimo, że zwracany typ () zgadza się z zadeklarowanym implicitly
-    // to funkcja "new" z interfejsu (traita) Sandbox MUSI zwracać Self
-    // (mieć dokładnie taką sygnaturę: "fn new() -> Self")
-
-    // zadanie domowe:
-    // figure out poprzez googlanie lub rozmowę z Chatem GPT
-    // zadając pytania "why" and "how" co tu się odpierdala,
-    // masz to rozumieć i wiedzieć co i jak i rozwikłać ten problem
-    // dzięki wiedzy, a nie dzięki znalezionym odpowiedziom.
-    // (najlepiej unikaj zaglądania do poprzedniego projektu,
-    //  raczej chcemy byś spróbowała się nauczyć sama od nowa)
-    
-    // Here is where you should return the initial state of your app.
     fn new() -> Self {
-        Calculator {
-            opening: "Ready to calculate".to_string(),
+        println!("new()");
+
+        Self {
+            text: String::new(),
+            theme: Theme::Dracula,
+            base_title: String::from("Printer"),
         }
     }
 
-    // no i usuń tutaj todo i zrób to samo co powyżej
-    fn title(&self) -> String {
-        "Kalkulator".to_string()
-    }
-
-    // i tu też
     fn update(&mut self, message: Self::Message) {
-        todo!()
+        println!("update({:?})", message);
+
+        use PrinterMessage::*;
+        if message == ChangeTheme {
+            if self.theme == Theme::Dracula {
+                self.theme = Theme::CatppuccinFrappe
+            } else {
+                self.theme = Theme::Dracula
+            }
+        } else if message == Print {
+            self.text.push_str("printu-sprintu ");
+        }
     }
 
-    // no i tu. :)
-    // Application View
-    // - how your app looks like
-    // - what's there? buttons, text fields, etc...
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        let button1 = widget::button("1").on_press(1);
+        println!("view()");
 
-        widget::container(button1).into()
+        use PrinterMessage::*;
+        let mut text = text(self.text.clone());
+        let mut btn_change_theme = button("Change Theme").on_press(ChangeTheme);
+        let mut btn_print = button("Print").on_press(Print);
 
+        column!(btn_change_theme, btn_print, text).into()
+    }
+
+    fn title(&self) -> String {
+        println!("title()");
+
+        let appendix = format!(" - with {} theme!", self.theme);
+
+        let mut new_title = self.base_title.clone();
+        new_title.push_str(&appendix);
+
+        new_title
     }
 
     fn theme(&self) -> Theme {
-        iced::Theme::SolarizedLight
-    }
+        println!("theme()");
 
-    // i skompiluj i uruchom, żeby zobaczyć czy się włącza
-    // nie musi nic robić, ma się pojawić okienko i tyle <3
+        self.theme.clone()
+    }
 }
